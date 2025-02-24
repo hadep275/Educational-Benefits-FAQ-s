e5a8fe6a8014932fba566e0a8062af9efd95f9d0
hadep275 <65734173+hadep275@users.noreply.github.com>
2025-02-23 20:00:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ fetch(url)
       
       const questionButton = document.createElement('button');
       questionButton.classList.add('faq-question');
-      questionButton.innerHTML = `<span class="faq-icon">+</span><span class="faq-text">${question}</span>`;
+      questionButton.innerHTML = `<span class="faq-text"><span class="faq-icon">+</span>${question}</span>`;
 
       const answerDiv = document.createElement('div');
       answerDiv.classList.add('faq-answer');
